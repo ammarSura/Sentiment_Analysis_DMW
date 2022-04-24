c84e88e583d1a24bc73e2f72623a089b9e18c8d7
ammarSura <ammarsura@gmail.com>
2022-04-24 11:43:19 +0530

    scraper completer

--- scraper.py
+++ scraper.py
@@ -2,74 +2,146 @@ from bs4 import BeautifulSoup
 import requests
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import json
 from time import sleep
 
-def foo(driver):
-    driver.get('https://www.agoda.com/en-in/')
-    sleep(5)
-
-    search = driver.find_element_by_css_selector("[data-selenium=textInput]")
-    search.send_keys('Bangkok')
-    search.send_keys(Keys.RETURN)
-    sleep(5)
-    driver.find_element_by_class_name("ab-close-button").click()
-    sleep(5)
-    searchButton = driver.find_element_by_css_selector("[data-selenium=searchButton]")
-    # searchButton.click()
 
 def getHotelURLs(url, driver): 
+    #gets urls of all hotels from query page
     driver.get(url)
-    sleep(5)
+    sleep(10)
 
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    sleep(10)
 
-    lst = driver.find_elements_by_css_selector("[data-selenium=hotel-item]")
-
+    lst = driver.find_elements(By.CSS_SELECTOR, "[data-selenium=hotel-item]")
+    
     hotelURLs = []
-
-    for i in lst:
-        hotelURLs.append(i.find_element_by_tag_name("a").get_attribute('href'))
+    
+    for i in range(len(lst)):
+        hotelURLs.append(lst[i].find_element(By.TAG_NAME, "a").get_attribute('href'))
     
     return hotelURLs
 
-def getRatings(url, driver):
+def getReviewsURL(url, driver):
+    #gets reviews url from hotel page
 
     driver.get(url)
+    sleep(5)
+    ele = False
+    try:
+        ele = driver.find_element(By.CSS_SELECTOR, '[data-selenium=review-basedon]')
+    except:
+        pass
     
-    return driver.find_element_by_class_name('Review__ReviewFormattedScore').text
+    if ele:
+        try:
+            return ele.find_element(By.TAG_NAME, "a").get_attribute('href')
+        except:
+            pass
+    
+    return False
+    
+    
 
 def getReviews(url, driver):
+    # gets reviews from hotel review page
 
     driver.get(url)
     sleep(2)
-    more_reviews = driver.find_element_by_class_name('Review-paginator-button')
-    more_reviews.click()
-    sleep(3)
+    hotel_name = driver.find_element(By.CSS_SELECTOR, '[data-selenium=hotel-header-name]').text
+    hotel_address = driver.find_element(By.CSS_SELECTOR, '[data-selenium=hotel-address-map]').text
+    
+    overall_rating = False
+    try:
+        overall_rating = driver.find_element(By.CLASS_NAME, "ReviewScore-Number ReviewScore-Number--line-height").text
+    except:
+        pass
 
-    reviewsHelper = driver.find_elements_by_class_name('Review-comment')
+    if not overall_rating:
+        try:
+            overall_rating = driver.find_element(By.CLASS_NAME, "Review__ReviewFormattedScore").text
+        except:
+            pass
+
+    if not overall_rating:
+        try:
+            overall_rating = driver.find_element(By.CLASS_NAME,"Typographystyled__TypographyStyled-sc-j18mtu-0 hTkvyT kite-js-Typography ").text
+        except:
+            pass
+
+
+    more_reviews = False
+    for i in range(5):
+        try:
+            more_reviews = driver.find_element(By.CLASS_NAME, "Review-paginator-button")
+        except:
+            break
+        if more_reviews:
+            more_reviews.click()
+            sleep(3)
+
+    reviewsHelper = driver.find_elements(By.CLASS_NAME, 'Review-comment')
+    if len(reviewsHelper) < 100:
+        return False
+
     reviews = []
-
-    for review in reviewsHelper:
-        title = review.find_element_by_class_name('Review-comment-bodyTitle').text
-        comment = review.find_element_by_class_name('Review-comment-bodyText').text
-        rating = review.find_element_by_class_name('Review-comment-leftScore').text
-        reviews.append((rating, title, comment))
-        # print(title, comment)
-
-    print(len(reviews), reviews[0])
     
 
+    for i in range(100):
+        title = reviewsHelper[i].find_element(By.CLASS_NAME, 'Review-comment-bodyTitle').text
+        comment = reviewsHelper[i].find_element(By.CLASS_NAME, 'Review-comment-bodyText').text
+        rating = reviewsHelper[i].find_element(By.CLASS_NAME, 'Review-comment-leftScore').text
+        reviews.append({
+            "review_rating" : rating,
+            "review_title" : title,
+            "review_comment" : comment 
+        })
 
+    hotel = {
+            "name" : hotel_name[:-8],
+            "address" : hotel_address,
+            "overall_rating" : overall_rating,
+            "reviews" : reviews
+        }
+
+
+    return hotel
+
+def scraper(queryURL, driver):
+    lst = []
+
+    hotelsURLS = getHotelURLs(queryURL, driver)
+    count = 1
+    for url1 in hotelsURLS:
+        url2 = getReviewsURL(url1, driver)
+        if url2:
+            hotel = getReviews(url2, driver)
+            if hotel:
+                print("Hotel", str(count) + ": ", hotel["name"])
+                count = count + 1
+            
+                lst.append(hotel)
+            if count > 20:
+                break
+
+    return lst
+
+    
 driver = webdriver.Chrome()
 
-queryURL = "https://www.agoda.com/en-in/search?checkin=2022-05-03&los=1&city=9395&adults=2&children=0&rooms=1&cid=1891461&tag=3cda3586-9ec8-88d2-2819-7b1e8bb3ad04"
+queryURL = input("Input Query String: ")
 
-eghotelURL = "https://www.agoda.com/en-in/grande-centre-point-hotel-ratchadamri/hotel/bangkok-th.html?finalPriceView=1&isShowMobileAppPrice=false&cid=1891461&numberOfBedrooms=&familyMode=false&adults=2&children=0&rooms=1&maxRooms=0&checkIn=2022-05-3&isCalendarCallout=false&childAges=&numberOfGuest=0&missingChildAges=false&travellerType=-1&showReviewSubmissionEntry=false&currencyCode=INR&isFreeOccSearch=false&tag=3cda3586-9ec8-88d2-2819-7b1e8bb3ad04&flexibleDateSearchCriteria=[object%20Object]&isCityHaveAsq=true&tspTypes=-1,-1&los=1&searchrequestid=26b91b3b-2cb1-4c62-a600-7fa28aa08f0f"
 
-eghotelRatingURL = "https://www.agoda.com/en-in/grande-centre-point-hotel-ratchadamri-sha-extra-plus/reviews/bangkok-th.html?finalPriceView=1&isShowMobileAppPrice=false&cid=1891461&numberOfBedrooms=&familyMode=false&adults=2&children=0&rooms=1&maxRooms=0&checkIn=2022-05-3&isCalendarCallout=false&childAges=&numberOfGuest=0&missingChildAges=false&travellerType=-1&showReviewSubmissionEntry=false&currencyCode=INR&isFreeOccSearch=false&tag=3cda3586-9ec8-88d2-2819-7b1e8bb3ad04&flexibleDateSearchCriteria=%5bobject%2520Object%5d&isCityHaveAsq=true&tspTypes=-1%2c-1&los=1&searchrequestid=26b91b3b-2cb1-4c62-a600-7fa28aa08f0f"
+lst = scraper(queryURL, driver)
+dic = { "hotels": lst }
+json_object = json.dumps(dic, indent = 4)
+  
+# Writing to sample.json
+with open("sample.json", "w") as outfile:
+    outfile.write(json_object)
 
-getReviews(eghotelRatingURL, driver)
 
 driver.close()
 driver.quit()
